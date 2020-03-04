3170a030af7ce3f477348075335d5fc31a1a636a
juxiang130 <juxiang130@outlook.com>
2020-03-04 09:16:17 +0800

    Third Commit

--- app/src/main/java/com/example/mad_practical_1/MainActivity.kt
+++ app/src/main/java/com/example/mad_practical_1/MainActivity.kt
@@ -3,10 +3,13 @@ package com.example.mad_practical_1
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var diceImage : ImageView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -14,18 +17,29 @@ class MainActivity : AppCompatActivity() {
 
         val rollButton: Button = findViewById(R.id.roll_button)
         val addButton: Button = findViewById(R.id.add_button)
+
+        diceImage = findViewById(R.id.dice_image)
+
         rollButton.setOnClickListener { rollDice() }
-        addButton.setOnClickListener{ add() }
+            //addButton.setOnClickListener{ add() }
     }
     private fun rollDice() {
-        val resultText: TextView = findViewById(R.id.result_text)
         val randomInt = (1..6).random()
-        resultText.text = randomInt.toString()
+        val drawableResource = when (randomInt) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+
+        diceImage.setImageResource(drawableResource)
         Toast.makeText(this, "button clicked",
             Toast.LENGTH_SHORT).show()
     }
 
-    private fun add(){
+    /*private fun add(){
         val resultText: TextView = findViewById(R.id.result_text)
         val value = resultText.text.toString().toIntOrNull()
         if(value == null){
@@ -34,5 +48,7 @@ class MainActivity : AppCompatActivity() {
         else if(value < 6) {
             resultText.text = (value + 1).toString()
         }
-    }
+    }*/
+
+
 }
